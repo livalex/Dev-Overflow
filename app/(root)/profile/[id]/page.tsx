9ef9f168725308bbdd0b5c8c8f5160bd077bae40
livalex <livadarualex@gmail.com>
2024-02-20 00:14:43 +0200

    fixed small css bug

--- app/(root)/profile/[id]/page.tsx
+++ app/(root)/profile/[id]/page.tsx
@@ -1,17 +1,19 @@
-import AnswersTab from "@/components/shared/AnswersTab";
-import ProfileLink from "@/components/shared/ProfileLink";
-import QuestionTab from "@/components/shared/QuestionTab";
-import Stats from "@/components/shared/Stats";
 import { Button } from "@/components/ui/button";
-import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
-import { getUserInfo } from "@/lib/actions/user.actions";
-import { getJoinedDate } from "@/lib/utils";
 import { URLProps } from "@/types";
 import { SignedIn, auth } from "@clerk/nextjs";
 import Image from "next/image";
 import Link from "next/link";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 
-const Profile = async ({ params, searchParams }: URLProps) => {
+import React from "react";
+import { getJoinedDate } from "@/lib/utils";
+import ProfileLink from "@/components/shared/ProfileLink";
+import Stats from "@/components/shared/Stats";
+import QuestionTab from "@/components/shared/QuestionTab";
+import AnswersTab from "@/components/shared/AnswersTab";
+import { getUserInfo } from "@/lib/actions/user.actions";
+
+const Page = async ({ params, searchParams }: URLProps) => {
   const { userId: clerkId } = auth();
   const userInfo = await getUserInfo({ userId: params.id });
 
@@ -26,6 +28,7 @@ const Profile = async ({ params, searchParams }: URLProps) => {
             height={140}
             className="rounded-full object-cover"
           />
+
           <div className="mt-3">
             <h2 className="h2-bold text-dark100_light900">
               {userInfo.user.name}
@@ -33,6 +36,7 @@ const Profile = async ({ params, searchParams }: URLProps) => {
             <p className="paragraph-regular text-dark200_light800">
               @{userInfo.user.username}
             </p>
+
             <div className="mt-5 flex flex-wrap items-center justify-start gap-5">
               {userInfo.user.portfolioWebsite && (
                 <ProfileLink
@@ -41,17 +45,20 @@ const Profile = async ({ params, searchParams }: URLProps) => {
                   title="Portfolio"
                 />
               )}
+
               {userInfo.user.location && (
                 <ProfileLink
                   imgUrl="/assets/icons/location.svg"
                   title={userInfo.user.location}
                 />
               )}
+
               <ProfileLink
                 imgUrl="/assets/icons/calendar.svg"
                 title={getJoinedDate(userInfo.user.joinedAt)}
               />
             </div>
+
             {userInfo.user.bio && (
               <p className="paragraph-regular text-dark400_light800 mt-8">
                 {userInfo.user.bio}
@@ -59,24 +66,27 @@ const Profile = async ({ params, searchParams }: URLProps) => {
             )}
           </div>
         </div>
+
         <div className="flex justify-end max-sm:mb-5 max-sm:w-full sm:mt-3">
           <SignedIn>
             {clerkId === userInfo.user.clerkId && (
               <Link href="/profile/edit">
                 <Button className="paragraph-medium btn-secondary text-dark300_light900 min-h-[46px] min-w-[175px] px-4 py-3">
-                  Edit profile
+                  Edit Profile
                 </Button>
               </Link>
             )}
           </SignedIn>
         </div>
       </div>
+
       <Stats
         totalQuestions={userInfo.totalQuestions}
         totalAnswers={userInfo.totalAnswers}
       />
+
       <div className="mt-10 flex gap-10">
-        <Tabs defaultValue="top-posts" className="w-[400px]">
+        <Tabs defaultValue="top-posts" className="flex-1">
           <TabsList className="background-light800_dark400 min-h-[42px] p-1">
             <TabsTrigger value="top-posts" className="tab">
               Top Posts
@@ -105,4 +115,4 @@ const Profile = async ({ params, searchParams }: URLProps) => {
   );
 };
 
-export default Profile;
+export default Page;
