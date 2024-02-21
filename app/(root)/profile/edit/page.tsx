eef91c56160d826a08b95ab3893291739b228d65
livalex <livadarualex@gmail.com>
2024-02-22 00:33:14 +0200

    edit profile completed

---
+++ app/(root)/profile/edit/page.tsx
@@ -0,0 +1,22 @@
+import Profile from "@/components/forms/Profile";
+import { getUserById } from "@/lib/actions/user.actions";
+import { auth } from "@clerk/nextjs";
+
+const EditProfile = async () => {
+  const { userId } = auth();
+
+  if (!userId) return null;
+
+  const mongoUser = await getUserById({ userId });
+
+  return (
+    <>
+      <h1 className="h1-bold text-dark100_light900">Edit Profile</h1>
+      <div className="mt-9">
+        <Profile clerkId={userId} mongoUser={JSON.stringify(mongoUser)} />
+      </div>
+    </>
+  );
+};
+
+export default EditProfile;
---
+++ components/forms/Profile.tsx
@@ -0,0 +1,186 @@
+"use client";
+
+import { ProfileSchema } from "@/lib/validations";
+import { useForm } from "react-hook-form";
+import * as z from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { Button } from "@/components/ui/button";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Textarea } from "@/components/ui/textarea";
+import { Input } from "@/components/ui/input";
+import { useState } from "react";
+import { usePathname, useRouter } from "next/navigation";
+import { updateUser } from "@/lib/actions/user.actions";
+
+interface Props {
+  clerkId: string;
+  mongoUser: string;
+}
+
+const Profile = ({ mongoUser, clerkId }: Props) => {
+  const parsedMongoUser = JSON.parse(mongoUser);
+  const [isSubmitting, setIsSubmitting] = useState(false);
+  const router = useRouter();
+  const pathName = usePathname();
+
+  const form = useForm<z.infer<typeof ProfileSchema>>({
+    resolver: zodResolver(ProfileSchema),
+    defaultValues: {
+      name: parsedMongoUser.name || "",
+      username: parsedMongoUser.username || "",
+      portfolioLink: parsedMongoUser.portfolioWebsite || "",
+      location: parsedMongoUser.location || "",
+      bio: parsedMongoUser.bio || "",
+    },
+  });
+
+  async function onSubmit(values: z.infer<typeof ProfileSchema>) {
+    setIsSubmitting(true);
+
+    try {
+      await updateUser({
+        clerkId,
+        updateData: {
+          name: values.name,
+          username: values.username,
+          portfolioWebsite: values.portfolioLink,
+          location: values.location,
+          bio: values.bio,
+        },
+        path: pathName,
+      });
+
+      router.back();
+    } catch (error) {
+      console.log(error);
+    } finally {
+      setIsSubmitting(false);
+    }
+    console.log(values);
+  }
+
+  return (
+    <Form {...form}>
+      <form
+        onSubmit={form.handleSubmit(onSubmit)}
+        className="mt-9 flex w-full flex-col gap-9"
+      >
+        <FormField
+          control={form.control}
+          name="name"
+          render={({ field }) => (
+            <FormItem className="flex w-full flex-col space-y-3.5">
+              <FormLabel className="paragraph-semibold text-dark400_light800">
+                Name <span className="text-primary-500">*</span>
+              </FormLabel>
+              <FormControl className="mt-3.5">
+                <Input
+                  placeholder="Your Name"
+                  className="no-focus paragraph-regular light-border-2 background-light800_dark300 text-dark300_light700 flex h-9 min-h-[56px] w-full rounded-md border border-slate-200 bg-transparent px-3 py-1 text-sm shadow-sm transition-colors file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-slate-500 focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-slate-950 disabled:cursor-not-allowed disabled:opacity-50 dark:border-slate-800 dark:placeholder:text-slate-400 dark:focus-visible:ring-slate-300"
+                  {...field}
+                />
+              </FormControl>
+              <FormMessage className="text-red-500" />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name="username"
+          render={({ field }) => (
+            <FormItem className="flex w-full flex-col space-y-3.5">
+              <FormLabel className="paragraph-semibold text-dark400_light800">
+                Username <span className="text-primary-500">*</span>
+              </FormLabel>
+              <FormControl className="mt-3.5">
+                <Input
+                  placeholder="Your Username"
+                  className="no-focus paragraph-regular light-border-2 background-light800_dark300 text-dark300_light700 flex h-9 min-h-[56px] w-full rounded-md border border-slate-200 bg-transparent px-3 py-1 text-sm shadow-sm transition-colors file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-slate-500 focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-slate-950 disabled:cursor-not-allowed disabled:opacity-50 dark:border-slate-800 dark:placeholder:text-slate-400 dark:focus-visible:ring-slate-300"
+                  {...field}
+                />
+              </FormControl>
+              <FormMessage className="text-red-500" />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name="portfolioLink"
+          render={({ field }) => (
+            <FormItem className="flex w-full flex-col space-y-3.5">
+              <FormLabel className="paragraph-semibold text-dark400_light800">
+                Portfolio Link
+              </FormLabel>
+              <FormControl className="mt-3.5">
+                <Input
+                  type="url"
+                  placeholder="Your Portfolio Link"
+                  className="no-focus paragraph-regular light-border-2 background-light800_dark300 text-dark300_light700 flex h-9 min-h-[56px] w-full rounded-md border border-slate-200 bg-transparent px-3 py-1 text-sm shadow-sm transition-colors file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-slate-500 focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-slate-950 disabled:cursor-not-allowed disabled:opacity-50 dark:border-slate-800 dark:placeholder:text-slate-400 dark:focus-visible:ring-slate-300"
+                  {...field}
+                />
+              </FormControl>
+              <FormMessage className="text-red-500" />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name="location"
+          render={({ field }) => (
+            <FormItem className="flex w-full flex-col space-y-3.5">
+              <FormLabel className="paragraph-semibold text-dark400_light800">
+                Location <span className="text-primary-500">*</span>
+              </FormLabel>
+              <FormControl className="mt-3.5">
+                <Input
+                  placeholder="Where do you live?"
+                  className="no-focus paragraph-regular light-border-2 background-light800_dark300 text-dark300_light700 flex h-9 min-h-[56px] w-full rounded-md border border-slate-200 bg-transparent px-3 py-1 text-sm shadow-sm transition-colors file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-slate-500 focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-slate-950 disabled:cursor-not-allowed disabled:opacity-50 dark:border-slate-800 dark:placeholder:text-slate-400 dark:focus-visible:ring-slate-300"
+                  {...field}
+                />
+              </FormControl>
+              <FormMessage className="text-red-500" />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name="bio"
+          render={({ field }) => (
+            <FormItem className="flex w-full flex-col space-y-3.5">
+              <FormLabel className="paragraph-semibold text-dark400_light800">
+                Bio <span className="text-primary-500">*</span>
+              </FormLabel>
+              <FormControl className="mt-3.5">
+                <Textarea
+                  rows={5}
+                  placeholder="What's special about you?"
+                  className="no-focus paragraph-regular light-border-2 background-light800_dark300 text-dark300_light700 flex min-h-[56px] w-full rounded-md border border-slate-200 bg-transparent px-3 py-2 text-sm shadow-sm placeholder:text-slate-500 focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-slate-950 disabled:cursor-not-allowed disabled:opacity-50 dark:border-slate-800 dark:placeholder:text-slate-400 dark:focus-visible:ring-slate-300"
+                  {...field}
+                />
+              </FormControl>
+              <FormMessage className="text-red-500" />
+            </FormItem>
+          )}
+        />
+        <div className="mt-7 flex justify-end">
+          <Button
+            type="submit"
+            className="primary-gradient w-fit !text-light-900"
+            disabled={isSubmitting}
+          >
+            {isSubmitting ? "Submitting..." : "Submit"}
+          </Button>
+        </div>
+      </form>
+    </Form>
+  );
+};
+
+export default Profile;
